e18ceb6361b46b3ac273186344dcb59a82e5fb28
Viktor Ivanov <sessgreem@gmail.com>
2018-07-01 13:37:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ function happyHolidays (){
   return "Happy holidays!";
 }
 function happyHolidaysTo(name){
-  console.log(`Happy holidays, ${name}!`);
+ return`Happy holidays, ${name}!`;
 }
 function happyHolidayTo(holiday,name){
-  console.log(`Happy ${holiday}, ${name}`);
+  return`Happy ${holiday}, ${name}`;
 }
 function holidayCountdown(holiday,days){
-  console.log(`It's ${days} days until ${holiday}!`);
-  
+  return`It's ${days} days until ${holiday}!`;
 }
